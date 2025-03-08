29891a64382980758b494e5b4b711146083a907f
1Huy2409 <nhathuy2409@gmail.com>
2025-03-08 19:12:04 +0700

    route

--- src/main/java/Application/HelloApplication.java
+++ src/main/java/Application/HelloApplication.java
@@ -8,24 +8,22 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/Shared/login.fxml")); // Đúng
-//        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-//        stage.setTitle("Food App!");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-@Override
-public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/Admin/category.fxml")); // Đúng
-    Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-    stage.setTitle("Food App!");
-    stage.setScene(scene);
-    stage.show();
-}
-
-
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/Shared/login.fxml")); // Đúng
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Food App!");
+        stage.setScene(scene);
+        stage.show();
+    }
+//@Override
+//public void start(Stage stage) throws IOException {
+//    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/Admin/category.fxml")); // Đúng
+//    Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+//    stage.setTitle("Food App!");
+//    stage.setScene(scene);
+//    stage.show();
+//}
 
     public static void main(String[] args) {
         launch();
